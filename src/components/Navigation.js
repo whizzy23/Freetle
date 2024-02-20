9b280f21aa4d6bbc3baa70d7df896b69b5e69d21
Pitamber Paudel <pitamberpaudel2003@gmail.com>
2024-02-21 01:07:32 +0530

    resolved some bugs

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -34,11 +34,8 @@ const handleNavbarExpand = () => {
             <Nav.Link className="mx-3"  onClick={handleNavbarCollapse} as={NavLink} to="about">About</Nav.Link> 
             <Nav.Link className="mx-3"  onClick={handleNavbarCollapse} as={NavLink} to="contact">Contact</Nav.Link>
             <Nav.Link className="d-md-none d-sm-flex"  onClick={handleNavbarCollapse} as={NavLink} to="/bookmarks">Bookmarks</Nav.Link>
-            <Nav.Link className="d-md-none d-sm-flex "  onClick={handleNavbarCollapse} as={NavLink} to="/signup">Log out</Nav.Link>
-          </Nav>
-          </Navbar.Collapse>
-          <Nav>
-          <NavDropdown title={<Image src={user} fluid width={27} />} className="user-section d-none d-md-flex mx-3 px-5" id="user-section">
+            <Nav.Link className="d-md-none d-sm-flex"  onClick={handleNavbarCollapse} as={NavLink} to="/signup">Log out</Nav.Link>
+            <NavDropdown title={<Image src={user} fluid width={27} />} className="user-section d-none d-md-flex px-5" id="user-section">
               <NavDropdown.Item as={NavLink} to="profile">Profile</NavDropdown.Item>
               <NavDropdown.Item as={NavLink} to="publications">Your Publications</NavDropdown.Item>
               <NavDropdown.Item as={NavLink} to="bookmarks">Bookmarks</NavDropdown.Item>
@@ -46,6 +43,7 @@ const handleNavbarExpand = () => {
               <NavDropdown.Item as={NavLink} to="signup">Log out</NavDropdown.Item>
             </NavDropdown>
           </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
